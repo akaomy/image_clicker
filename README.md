60531ffdd566cd52b647b4ce1f3e16ab840d9a5b
akozlova <annakozlova92@gmail.com>
2019-06-13 15:21:00 -0700

    initialize MVC pattern for the project

--- README.md
+++ README.md
@@ -2,15 +2,20 @@
 When an image name is clicked in the list, the image display area should update to show the data for the selected image.
 The number of clicks in the image area should be unique to each image, and should increment when the image is clicked.
 
-## The application should display
+# The application should display
 
-- a list of at least 5 images, listed by name
+- a list of cats by name
 - an area to display the selected image
 
-## In the image display area, the following should be displayed
+*In the cat display area, the following should be displayed:*
 
 - the image's name
 - an image
 - text showing the number of clicks
 
 There are no specific styling requirements
+
+# Interaction
+
+- When a cat name is clicked in the list, the cat display area should update to show the data for the selected cat.
+- The number of clicks in the cat area should be unique to each cat, and should increment when the cat's picture is clicked.
--- main.js
+++ main.js
@@ -1,79 +1,80 @@
-let cat1Img = document.querySelector('.cat1Img');
-let cat1Click = document.querySelector('.cat1Click');
-let catName1 = document.querySelector('.cat-name1');
-let count1 = 0;
+let model = {
+  currentImg: null,
+  imgs: [
+    {
+      clickCount: 0,
+      name : 'nature',
+      imgSrc : 'https://placeimg.com/640/480/nature'
+    },
+    {
+      clickCount: 0,
+      name : 'animals',
+      imgSrc : 'https://placeimg.com/640/480/animals'
+    },
+    {
+      clickCount: 0,
+      name : 'arch',
+      imgSrc : 'https://placeimg.com/640/480/arch'
+    },
+    {
+      clickCount: 0,
+      name : 'people',
+      imgSrc : 'https://placeimg.com/640/480/people'
+    },
+    {
+      clickCount: 0,
+      name : 'tech',
+      imgSrc : 'https://placeimg.com/640/480/tech'
+    }
+  ]
+}
 
-let cat2Img = document.querySelector('.cat2Img');
-let cat2Click = document.querySelector('.cat2Click');
-let catName2 = document.querySelector('.cat-name2');
-let count2 = 0;
+let controller = {
 
-let cat3Img = document.querySelector('.cat3Img');
-let cat3Click = document.querySelector('.cat3Click');
-let catName3 = document.querySelector('.cat-name3');
-let count3 = 0;
+  init: function () {
 
-let cat4Img = document.querySelector('.cat4Img');
-let cat4Click = document.querySelector('.cat4Click');
-let catName4 = document.querySelector('.cat-name4');
-let count4 = 0;
+  },
 
-let cat5Img = document.querySelector('.cat5Img');
-let cat5Click = document.querySelector('.cat5Click');
-let catName5 = document.querySelector('.cat-name5');
-let count5 = 0;
+  getCurrentImg: function () {
 
-cat1Img.addEventListener('click', function() {
-  console.log('click');
-  count1 += 1;
-  cat1Click.innerText = count1;
-});
+  },
 
-cat2Img.addEventListener('click', function() {
-  console.log('click');
-  count2 += 1;
-  cat2Click.innerText = count2;
-});
+  getImage: function () {
 
-cat3Img.addEventListener('click', function() {
-  console.log('click');
-  count3 += 1;
-  cat3Click.innerText = count3;
-});
+  },
 
-cat4Img.addEventListener('click', function() {
-  console.log('click');
-  count4 += 1;
-  cat4Click.innerText = count4;
-});
+  setCurrentImg: function () {
 
-cat5Img.addEventListener('click', function() {
-  console.log('click');
-  count5 += 1;
-  cat5Click.innerText = count5;
-});
+  },
 
-let cat1Container = document.querySelector('.cat1');
-let paragraph1 = document.createElement('p');
-paragraph1.textContent = "Vasya";
-cat1Container.appendChild(paragraph1);
+  incrementCounter: function () {
 
-let cat2Container = document.querySelector('.cat2');
-let paragraph2 = document.createElement('p');
-paragraph2.textContent = "Petya";
-cat2Container.appendChild(paragraph2);
+  }
+}
 
-let cat3Container = document.querySelector('.cat3');
-let paragraph3 = document.createElement('p');
-paragraph3.textContent = "Jack";
-cat3Container.appendChild(paragraph3);
+let imageView = {
 
-let cat4Container = document.querySelector('.cat4');
-let paragraph4 = document.createElement('p');
-paragraph4.textContent = "Petya";
-cat4Container.appendChild(paragraph4);
+  init: function () {
 
-let cat5Container = document.querySelector('.cat5');
-let paragraph5 = document.createElement('p');
-paragraph5.textContent = "Petya";
-cat5Container.appendChild(paragraph5);
+  },
+
+  render: function () {
+
+  }
+
+}
+
+let imageListView = {
+
+  init: function () {
+
+  },
+
+  render: function () {
+
+  }
+
+}
+
+
+controller.init();
